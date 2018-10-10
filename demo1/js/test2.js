1d749a42b75d50d9df475b77e98ece096443f087
zhaoxuan <zhaoxuan1202@163.com>
2018-10-10 10:34:36 +0800

    test

--- demo1/js/test2.js
+++ demo1/js/test2.js
@@ -21,6 +21,24 @@ document.getElementsByClassName('button')[0].addEventListener("click", function(
   window.alert(1)
   console.log(1)
 });
-document.getElementsByClassName('button')[0].addEventListener("mouseover", function(e){
-  btn_move(document.getElementsByClassName('button')[0], e.clientX, e.clientY);
-});
+document.getElementsByClassName('button')[0].onmouseover = over_btn
+
+let domText = '<div>小选择的豆腐脑</div>'
+document.body.insertBefore(document.querySelector('.mod_container'), domText)
+
+
+function httpRequest(url, callback){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+          callback(xhr.responseText);
+      }
+  }
+  xhr.send();
+}
+let searchurl = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=jd.com&prop=info&inprop=url&utf8=&format=json'
+function searchCallBack (res) {
+  console.log(res.query.search)
+}
+httpRequest(searchurl, searchCallBack)
